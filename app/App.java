3783308932aba82afc0a469641c642d5f3654706
Parag Gautam <paraggautam123@gmail.com>
2026-05-20 17:50:30 +0530

    Added correct code

--- app/App.java
+++ app/App.java
@@ -1,12 +1,5 @@
-// public class App {
-//     public static void main(String[] args) {
-//         System.out.println("Enterprise CI/CD Pipeline");
-//     }
-// }
 public class App {
     public static void main(String[] args) {
-        System.out.println("Enterprise CI/CD Pipeline")
-
-        invalid_code
+        System.out.println("Enterprise CI/CD Pipeline");
     }
 }
